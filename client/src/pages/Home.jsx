1dc58cc77c9cd3b56f1404046a4a7f6526a82565
Kevin Ekart <kevin.ekart@gmail.com>
2022-10-10 12:00:34 -0400

    added tailwind and styled the home page

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -37,10 +37,19 @@ const Home = () => {
     }
 
     return (
-        <div className="home-container">
-            <input type="text" placeholder="Game Code" value={roomId} onChange={handleRoomNameChange} className="text-input-field" />
-            <button onClick={handleJoinGame} className="join-game">Join Game</button>
-            <button onClick={handleNewGame} className="new-game">New Game</button>
+        <div class="grid h-screen place-items-center">
+            <form class="px-8 pt-6 pb-8 mb-4">
+                <h1 className="text-7xl font-sans font-normal leading-normal mt-0 mb-2 text-white">
+                    Worduel
+                </h1>
+                <div class="mb-4">
+                    <input type="text" placeholder="Game Code" value={roomId} onChange={handleRoomNameChange} class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" />
+                </div>
+                <div class="flex items-center justify-between">
+                    <button onClick={handleJoinGame} class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Join Game</button>
+                    <button onClick={handleNewGame} class="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 ml-2 rounded">New Game</button>
+                </div>
+            </form>
         </div>
     );
 };
